592f4c661285aa41270c265caf953280b3851298
谢树宏 <384180258@qq.com>
2022-04-14 15:35:37 +0800

    feat: 增加

---
+++ Node/study/buf1.js
@@ -0,0 +1,13 @@
+const buf = new Buffer("test", "utf-8");
+console.log(buf);
+
+const fs = require("fs");
+var rs = fs.createReadStream("./test.md", { highWaterMark: 11 });
+rs.setEncoding("utf8");
+let data = "";
+rs.on("data", function (chunk) {
+  data += chunk;
+});
+rs.on("end", function () {
+  console.log(data);
+});
