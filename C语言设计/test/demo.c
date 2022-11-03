571cd0ce937cf27bf4e5cac50002a13bd56a2323
谢树宏 <384180258@qq.com>
2022-11-03 19:44:18 +0800

    feat: 增加

--- C语言设计/test/demo.c
+++ C语言设计/test/demo.c
@@ -2,23 +2,11 @@
 
 int main()
 {
-  int max(int x, int y);
-  int a, b, c;
-  scanf("%d,%d", &a, &b);
-  c = max(a, b);
-  printf("max=%d\n", c);
-}
+  int a = 1;
+  int b = 4;
+  int c;
+  c = a / b;
+  printf("%d\n", c);
 
-int max(int x, int y)
-{
-  int z;
-  if (x > y)
-  {
-    z = x;
-  }
-  else
-  {
-    z = y;
-  }
-  return z;
+  return (0);
 }
