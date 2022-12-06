21e6541c7df6282e88baa3f4f629e0a622f2675c
谢树宏 <384180258@qq.com>
2022-12-06 18:03:56 +0800

    feat: 增加

--- C语言数据结构/test/tempCodeRunnerFile.c
+++ C语言数据结构/test/tempCodeRunnerFile.c
@@ -1,16 +1,9 @@
-if (i < 1 || i > l.length + 1)
+while (p->next)
   // {
-  //   printf("超过index");
-  //   return;
+  //   printf("---");
+  //   if (p->data != NULL)
+  //   {
+  //     printf("%s\n", p->data->name);
+  //   }
+  //   p = p->next;
   // }
-  // if (l.length == MAXSIZE)
-  // {
-  //   return;
-  // }
-
-  // for (j = l.length - 1; j >= i - 1; j--)
-  // {
-  //   l.elem[j + 1] = l.elem[j];
-  // }
-  // l.elem[i - 1] = e;
-  // ++l.length;
